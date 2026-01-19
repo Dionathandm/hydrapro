3a53308d6cd9340753ffd1b1623d32dd26a682bf
Dionathandm <plugplaysong@gmail.com>
2026-01-19 02:06:24 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,38 @@
+const CACHE_NAME = "hydra-v2";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./sw.js"
+];
+
 self.addEventListener("install", event => {
   self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
 });
 
 self.addEventListener("activate", event => {
-  clients.claim();
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    fetch(event.request).catch(() => {
-      return new Response("Offline", {
-        headers: { "Content-Type": "text/plain" }
-      });
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });

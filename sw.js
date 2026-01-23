81a86b7be1a8d86d3b2795437185d1a1292a3594
Dionathandm <plugplaysong@gmail.com>
2026-01-23 04:45:48 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,26 @@
-const CACHE_NAME = "hydrapro-cache-v1";
+const CACHE_NAME = 'hydrapro-cache-v1';
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/privacy.html",
-  "/manifest.json",
-  "/lv_0_20260122231858.mp4",
-  "https://i.ibb.co/0VBBMfrZ/lv-0-20260119011237.png",
-  "https://i.ibb.co/r22ZV0kv/lv-0-20260116051448.png",
-  "https://i.ibb.co/60KP16PJ/lv-0-20260117003207.png"
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/lv_0_20260122231858.mp4',
+  '/sw.js',
+  'https://i.ibb.co/kVqmYyzv/icon-512.png',
+  'https://i.ibb.co/0VBBMfrZ/lv-0-20260119011237.png',
+  'https://i.ibb.co/r22ZV0kv/lv-0-20260116051448.png',
+  'https://i.ibb.co/60KP16PJ/lv-0-20260117003207.png'
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
   );
 });
